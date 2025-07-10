1b3ebb3eb9d174576181c0e4d6e8055af353d02d
Yousef Rabi <yousofrabi@gmail.com>
2025-07-11 09:03:14 +1000

    add while true for server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,26 +11,28 @@ fun main() {
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
 
-    val clientConn = serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
-    val inputStream = clientConn.getInputStream()
-    val reader = inputStream.bufferedReader()
-    val requestLine = reader.readLine() ?: ""
-    val requestTarget = requestLine.split(" ").getOrNull(1) ?: ""
-    println("requestTarget: $requestTarget")
+    while (true) {
+        val clientConn = serverSocket.accept() // Wait for connection from client.
+        println("accepted new connection")
+        val inputStream = clientConn.getInputStream()
+        val reader = inputStream.bufferedReader()
+        val requestLine = reader.readLine() ?: ""
+        val requestTarget = requestLine.split(" ").getOrNull(1) ?: ""
+        println("requestTarget: $requestTarget")
 
-    val response = when {
-        requestTarget == "/" -> "HTTP/1.1 200 OK\r\n\r\n"
-        requestTarget.startsWith("/echo/") -> {
-            val echoedStr = requestTarget.substringAfter("/echo/")
-            val headers = "Content-Type: text/plain\r\nContent-Length: ${echoedStr.toByteArray().size}\r\n"
-            "HTTP/1.1 200 OK\r\n${headers}\r\n$echoedStr"
+        val response = when {
+            requestTarget == "/" -> "HTTP/1.1 200 OK\r\n\r\n"
+            requestTarget.startsWith("/echo/") -> {
+                val echoedStr = requestTarget.substringAfter("/echo/")
+                val headers = "Content-Type: text/plain\r\nContent-Length: ${echoedStr.toByteArray().size}\r\n"
+                "HTTP/1.1 200 OK\r\n${headers}\r\n$echoedStr"
+            }
+            else -> "HTTP/1.1 404 Not Found\r\n\r\n"
         }
-        else -> "HTTP/1.1 404 Not Found\r\n\r\n"
-    }
-    println("response: $response")
+        println("response: $response")
 
-    clientConn.getOutputStream().write(response.toByteArray())
-    println("sent simple response")
-    clientConn.close()
+        clientConn.getOutputStream().write(response.toByteArray())
+        println("sent simple response")
+        clientConn.close()
+    }
 }
